607f3d79a15dbebd1921a85cd2b90858f7085d35
Yuan He <yuanhe777tt@hotmail.com>
2020-05-25 16:11:44 -0400

    Update

--- simulation/Generate_input.R
+++ simulation/Generate_input.R
@@ -171,7 +171,8 @@ generate_input <- function(N=100, P = 10, K=5, saveplot = F, tau, savedir, seed)
 
   tempfn=paste0(savedir, '/X_tau', tau, '_seed', seed, '.mat')
   print(tempfn)
-  writeMat(paste0(savedir, '/X_tau', tau, '_seed', seed, '.mat'), X = X)
+  tp = X[, seq(3, ncol(X))]
+  writeMat(paste0(savedir, '/X_tau', tau, '_seed', seed, '.mat'), X = tp)
   save(X, f, l, W, file = paste0(savedir, '/Input_tau', tau, '_seed', seed, '.RData'))
   write.table(X, paste0(savedir, '/Input_tau', tau, '_seed', seed, '_X.txt'), sep='\t', quote = F, row.names = F)
   write.table(W, paste0(savedir, '/Input_tau', tau, '_seed', seed, '_W.txt'), sep='\t', quote = F, row.names = F)
--- simulation/nsfa-master/code/run_run_nsfa_mydata.sh
+++ simulation/nsfa-master/code/run_run_nsfa_mydata.sh
@@ -2,11 +2,8 @@
 
 
 ml matlab
-for tau in 1000 500 100 20 10 5 1
-do 
-	for seed in {1..10}
-	do
-		echo "tau${tau}_seed${seed}"
-		matlab -r "run_nsfa_mydata($tau, $seed);exit(0)"
-	done
-done
+tau="$1"
+seed="$2"
+
+echo "tau${tau}_seed${seed}"
+matlab -r "run_nsfa_mydata($tau, $seed);exit(0)"
