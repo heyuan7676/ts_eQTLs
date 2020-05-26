1bcc71eec6bf6127d8632b965da52eb60c591da8
Yuan He <yuanhe777tt@hotmail.com>
2020-05-26 14:47:08 -0400

    Plot

--- simulation/plot_metrics.R
+++ simulation/plot_metrics.R
@@ -6,12 +6,16 @@ suppressWarnings(library(cowplot))
 
 
 metrics = read.table('simulation/output/metrics/metrics.txt', sep=' ', header=T, stringsAsFactors = F)
+metrics = metrics[metrics$tau != 1, ]
 metrics$method = factor(metrics$method, 
                            levels = c("sn_spMF", "flashr_nn", "flashr_backfit","flashr_default",
                                       "softImpute", "PCA", "SSVD", 
-                                      "PMA_cv1", "PMA_cv2","NMF"))
+                                      "PMA_cv1", "PMA_cv2","NMF", "NBSFA"))
 metrics$tau = factor(metrics$tau)
-metrics[is.na(metrics$precision), "precision"] = 0
+metrics[is.na(metrics$u_precision), "u_precision"] = 0
+metrics[is.na(metrics$u_recall), "u_recall"] = 0
+metrics[is.na(metrics$ts_precision), "ts_precision"] = 0
+metrics[is.na(metrics$ts_recall), "ts_recall"] = 0
 
 
 evaluation_theme =  theme(axis.title.x = element_blank(),
@@ -25,7 +29,7 @@ mycolors <- colorRampPalette(brewer.pal(8, "Set2"))(12)
 f_corr_plot = ggplot(data = metrics, aes(tau, f_corr, fill = method)) + 
   geom_boxplot(lwd=0.2, outlier.size = 0.2) + 
   ggtitle("Correlation with true factor values") + 
-  xlab("Precision of the error values") + 
+  xlab("") + 
   ylab("") + 
   scale_fill_manual(values = mycolors) + 
   evaluation_theme
@@ -33,36 +37,57 @@ f_corr_plot = ggplot(data = metrics, aes(tau, f_corr, fill = method)) +
 l_corr_plot = ggplot(data = metrics, aes(tau, l_corr, fill = method)) + 
   geom_boxplot(lwd=0.2, outlier.size = 0.2) + 
   ggtitle("Correlation with true loading values") + 
+  xlab("") + 
+  ylab("") + 
+  scale_fill_manual(values = mycolors) + 
+  evaluation_theme
+
+u_precision_plot = ggplot(data = metrics, aes(tau, u_precision, fill = method)) + 
+  geom_boxplot(lwd=0.2, outlier.size = 0.2) + 
+  ggtitle("Precision of u-eQTLs") + 
+  xlab("") + 
+  ylab("") + 
+  scale_fill_manual(values = mycolors) + 
+  evaluation_theme
+
+u_recall_plot = ggplot(data = metrics, aes(tau, u_recall, fill = method)) + 
+  geom_boxplot(lwd=0.2, outlier.size = 0.2) + 
+  ggtitle("Recall of u-eQTLs") + 
+  xlab("") + 
+  ylab("") + 
+  scale_fill_manual(values = mycolors) + 
+  evaluation_theme
+
+
+
+ts_precision_plot = ggplot(data = metrics, aes(tau, ts_precision, fill = method)) + 
+  geom_boxplot(lwd=0.2, outlier.size = 0.2) + 
+  ggtitle("Precision of ts-eQTLs") + 
   xlab("Precision of the error values") + 
   ylab("") + 
   scale_fill_manual(values = mycolors) + 
   evaluation_theme
 
-precision_plot = ggplot(data = metrics, aes(tau, precision, fill = method)) + 
+ts_recall_plot = ggplot(data = metrics, aes(tau, ts_recall, fill = method)) + 
   geom_boxplot(lwd=0.2, outlier.size = 0.2) + 
-  ggtitle("Precision") + 
+  ggtitle("Recall of ts-eQTLs") + 
   xlab("Precision of the error values") + 
   ylab("") + 
   scale_fill_manual(values = mycolors) + 
   evaluation_theme
 
-recall_plot = ggplot(data = metrics, aes(tau, recall, fill = method)) + 
-  geom_boxplot(lwd=0.2, outlier.size = 0.2) + 
-  ggtitle("Recall") + 
-  xlab("Precision of the error values") + 
-  ylab("") + 
-  scale_fill_manual(values = mycolors) + 
-  evaluation_theme
 
 
 
 g = ggdraw() + 
-  draw_plot(f_corr_plot + theme(legend.position = 'none'), x = 0, y = 0.5, width = 0.4, height = 0.5)  + 
-  draw_plot(l_corr_plot, x = 0.45, y = 0.5, width = 0.5, height = 0.5) +
-  draw_plot(recall_plot + theme(legend.position = 'none'), x = 0, y = 0, width = 0.4, height = 0.5) +
-  draw_plot(precision_plot + theme(legend.position = 'none'), x = 0.45, y = 0, width = 0.4, height = 0.5)
+  draw_plot(f_corr_plot + theme(legend.position = 'none'), x = 0, y = 0.66, width = 0.4, height = 0.33)  + 
+  draw_plot(l_corr_plot + theme(legend.position = 'none'), x = 0.45, y = 0.66, width = 0.4, height = 0.33) +
+  draw_plot(u_recall_plot + theme(legend.position = 'none'), x = 0, y = 0.33, width = 0.4, height = 0.33) +
+  draw_plot(u_precision_plot, x = 0.45, y = 0.33, width = 0.485, height = 0.33)+
+  draw_plot(ts_recall_plot + theme(legend.position = 'none'), x = 0, y = 0, width = 0.4, height = 0.33)+
+  draw_plot(ts_precision_plot + theme(legend.position = 'none'), x = 0.45, y = 0, width = 0.4, height = 0.33)
 
-save_plot("simulation/output/metrics/metrics.png", g, base_width = 12, base_height = 6)
+save_plot("simulation/output/metrics/metrics.png", g, base_width = 16, base_height = 10)
 
 
 
