4cc80c497031b1623f53a47575b858c48d564c48
Yuan He <yuanhe777tt@hotmail.com>
2020-05-27 19:07:08 -0400

    Update

--- sn_spMF/tune_parameters_preliminary.R
+++ sn_spMF/tune_parameters_preliminary.R
@@ -1,5 +1,3 @@
-source("sn_spMF/cophenet.R")
-source("sn_spMF/collect_results.R")
 suppressWarnings(library(plyr))
 suppressWarnings(library(optparse))
 
@@ -8,12 +6,27 @@ option_list = list(make_option(c("-O", "--outputdir"), type = "character", defau
 opt = parse_args(OptionParser(option_list=option_list))
 
 
+collect_results_preliminary <- function(outputdir, K, alpha1, lambda1){
+        # read in the arguments
+        Fn = paste0('K', K, '_a1', alpha1, '_l1', lambda1);
+        outputdir = file.path(outputdir, paste0('sn_spMF_', Fn));
+	outputFn = paste0(outputdir, '/sn_spMF_', Fn, '.RData');
+
+	load(outputFn);
+	factor_corr = cor(FactorM)
+	factor_corr = norm(factor_corr, 'F');
+
+	return(c(K, alpha1, lambda1, factor_corr, ncol(FactorM), L_sparsity, F_sparsity))
+                                                                       
+}
+
+
 
 result = NULL
 for(K in c(10, 15, 20)){
         for(alpha1 in c(1,10,100,500)){
                 for(lambda1 in c(1,10,100,500)){
-                        rowi = collect_results(opt$outputdir, K, alpha1, lambda1)
+                        rowi = collect_results_preliminary(opt$outputdir, K, alpha1, lambda1)
                         if(!is.null(rowi)){
                                 result = rbind(result, rowi)
                         }
@@ -22,8 +35,7 @@ for(K in c(10, 15, 20)){
 }
 
 result = as.data.frame(result)
-colnames(result) = c("K", "alpha1", "lambda1", "coph", "correlation", "nfactor", "optimal_run", "optimal_obj")
-result = result[order(result$coph), ]
+colnames(result) = c("K", "alpha1", "lambda1", "correlation", "nfactor", "L_sparsity", "F_sparsity")
 write.table(result, paste0(opt$outputdir, opt$savefn), sep='\t', quote = F, row.names = F)
 
 
